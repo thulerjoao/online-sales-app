2f83f0987bfa27fd451dc19f17533adcb4b873d7
thulerjoao <thuler_lima@hotmail.com>
2023-05-26 02:25:07 -0300

    feat: created shared button

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -1,14 +1,20 @@
 import { Text, View } from 'react-native';
 
+import Button from '../../../shared/components/button/Button';
 import Input from '../../../shared/components/input/input';
 import { LoginContainer } from '../styles/login.styles';
 
 const Login = () => {
+  const handleOnPress = () => {
+    console.log('Clicou aqui!');
+  };
+
   return (
     <View>
       <LoginContainer>
         <Text>Login</Text>
         <Input />
+        <Button title="Entrar" margin="16px" onPress={handleOnPress} />
       </LoginContainer>
     </View>
   );
--- src/shared/components/input/input.tsx
+++ src/shared/components/input/input.tsx
@@ -1,4 +1,4 @@
-import { TextInputProps } from 'react-native';
+import { TextInputProps } from 'react-native/types';
 
 import { InputContainer } from './input.style';
 
