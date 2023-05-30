f34b2f5feeb7730f53df40a636211cdfe8095b11
thulerjoao <thuler_lima@hotmail.com>
2023-05-30 19:50:01 -0300

    feat: created connection with api

--- src/modules/login/hooks/useLogin.ts
+++ src/modules/login/hooks/useLogin.ts
@@ -1,6 +1,7 @@
-import axios from 'axios';
 import { useState } from 'react';
 import { NativeSyntheticEvent, TextInputChangeEventData } from 'react-native/types';
+
+import { ConnectionApiPost } from '../../../shared/functions/connection/connectionApi';
 
 export const useLogin = () => {
   const [email, setEmail] = useState<string>('user@user.com');
@@ -11,13 +12,11 @@ export const useLogin = () => {
   const handleOnPress = async () => {
     setLoading(true);
     console.log('Clicou aqui!');
-    await axios
-      .post('http://192.168.1.107:8080/auth', {
-        email: email,
-        password: password,
-      })
-      .then((res) => {
-        console.log(res.data);
+    await ConnectionApiPost('http://192.168.1.107:8080/auth', {
+      email: email,
+      password: password,
+    })
+      .then(() => {
         console.log('fiz login');
         setLoading(false);
       })
---
+++ src/shared/functions/connection/connectionApi.ts
@@ -0,0 +1,55 @@
+import axios from 'axios';
+
+import { MethodEnum } from '../../enums/methods.enum';
+
+export type MethodoType = 'get' | 'delete' | 'post' | 'put' | 'patch';
+
+export default class ConnectionApi {
+  static async call<T>(url: string, method: MethodoType, body?: unknown): Promise<T> {
+    switch (method) {
+      case MethodEnum.GET:
+      case MethodEnum.DELETE:
+        return (await axios[method]<T>(url)).data;
+      case MethodEnum.PATCH:
+      case MethodEnum.POST:
+      case MethodEnum.PUT:
+        return (await axios[method]<T>(url, body)).data;
+    }
+  }
+
+  static async connect<T>(url: string, method: MethodoType, body?: unknown): Promise<T> {
+    return this.call<T>(url, method, body).catch((error) => {
+      if (error.response) {
+        switch (error.response.stauts) {
+          case 401:
+          case 403:
+            throw new error('Sem permissão');
+
+          default:
+            throw new error('Sem conexão com a API');
+        }
+      }
+      throw new error('Sem conexão com a API');
+    });
+  }
+}
+
+export const ConnectionApiGet = async <T>(url: string): Promise<T> => {
+  return ConnectionApi.connect(url, MethodEnum.GET);
+};
+
+export const ConnectionApiDelete = async <T>(url: string): Promise<T> => {
+  return ConnectionApi.connect(url, MethodEnum.DELETE);
+};
+
+export const ConnectionApiPost = async <T>(url: string, body: unknown): Promise<T> => {
+  return ConnectionApi.connect(url, MethodEnum.POST, body);
+};
+
+export const ConnectionApiPut = async <T>(url: string, body: unknown): Promise<T> => {
+  return ConnectionApi.connect(url, MethodEnum.PUT, body);
+};
+
+export const ConnectionApiPatch = async <T>(url: string, body: unknown): Promise<T> => {
+  return ConnectionApi.connect(url, MethodEnum.PATCH, body);
+};
