eae0efabefb42c15a1142f035e61761ed5626a14
thulerjoao <thuler_lima@hotmail.com>
2023-06-26 00:17:37 -0300

    feat: finished unit test in cartREducer hook

--- src/store/reduces/cartReducer/__tests__/useCartReducer.spec.ts
+++ src/store/reduces/cartReducer/__tests__/useCartReducer.spec.ts
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-native';
+import { act, renderHook } from '@testing-library/react-native';
 
 import { mockCart } from '../__mocks__/cart.mock';
 import { useCartReducer } from '../useCartReducer';
@@ -15,10 +15,23 @@ jest.mock('../../../hooks', () => ({
   }),
 }));
 
+jest.mock('..', () => ({
+  setCartAction: () => null,
+}));
+
 describe('Usecart reducer test', () => {
   const { result } = renderHook(() => useCartReducer());
 
   it('Should return cart', () => {
     expect(result.current.cart).toEqual(mockCart);
   });
+
+  it('Should run setCart', () => {
+    act(() => {
+      result.current.setCart(mockCart);
+    });
+    
+    expect(mockDispatch).toBeCalled();
+  });
+
 });
