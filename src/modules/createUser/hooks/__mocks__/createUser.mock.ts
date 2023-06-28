6941207cb5066308425925e5f845d13ef385f49d
thulerjoao <thuler_lima@hotmail.com>
2023-06-27 23:47:21 -0300

    feat: implemented unit test in create new user inputs

---
+++ src/modules/createUser/hooks/__mocks__/createUser.mock.ts
@@ -0,0 +1,10 @@
+import { CreateUserType } from "../../../../shared/types/types";
+
+export const mockCreateUser: CreateUserType = {
+    name: 'name',
+    email: 'email@email.com',
+    cpf: '15019718750',
+    phone: '2225227864',
+    password: 'Abc123!',
+    confirmPassword: 'Abc123!'
+}
--- src/modules/createUser/hooks/__tests__/useCreateUser.spec.ts
+++ src/modules/createUser/hooks/__tests__/useCreateUser.spec.ts
@@ -1,6 +1,7 @@
 import { act, renderHook } from '@testing-library/react-native';
 import { DEFAULT_CREATE_USER, useCreateUser } from '../useCreateUser';
 import { useNavigation } from '@react-navigation/native';
+import { mockCreateUser } from '../__mocks__/createUser.mock';
 
 const mockReset = jest.fn();
 const mockRequest = jest.fn();
@@ -40,4 +41,59 @@ describe('UseCreateUser', () => {
 
     expect(result.current.createUser.cpf).toEqual(mockTest);
   });
+
+  it('Should set disable after insert all data',() => {
+    const { result } = renderHook(() => useCreateUser());
+
+    act(() => {
+      result.current.handleOnChangeInput({nativeEvent: {
+        text: mockCreateUser.confirmPassword,
+      }} as any, 'confirmPassword');
+    });
+
+    expect(result.current.disable).toEqual(true);
+
+    act(() => {
+      result.current.handleOnChangeInput({nativeEvent: {
+        text: mockCreateUser.password,
+      }} as any, 'password');
+    });
+
+    expect(result.current.disable).toEqual(true);
+
+    act(() => {
+      result.current.handleOnChangeInput({nativeEvent: {
+        text: mockCreateUser.cpf,
+      }} as any, 'cpf');
+    });
+
+    expect(result.current.disable).toEqual(true);
+
+    act(() => {
+      result.current.handleOnChangeInput({nativeEvent: {
+        text: mockCreateUser.name,
+      }} as any, 'name');
+    });
+
+    expect(result.current.disable).toEqual(true);
+
+    act(() => {
+      result.current.handleOnChangeInput({nativeEvent: {
+        text: mockCreateUser.email,
+      }} as any, 'email');
+    });
+
+    expect(result.current.disable).toEqual(true);
+
+    act(() => {
+      result.current.handleOnChangeInput({
+        nativeEvent: {
+        text: mockCreateUser.phone,
+      }} as any, 'phone');
+    });
+
+    expect(result.current.disable).toEqual(false);
+
+  })
+
 });
